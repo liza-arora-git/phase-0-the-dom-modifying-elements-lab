686c477b24b96883be15b09744709628786da719
Liza Arora <liza.arora@outlook.com>
2022-05-11 01:25:19 +1000

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+const element = document.getElementById('main');
+element.remove();
+
+// document.body.onload = addElement;
+
+// function addElement(){
+//     const newHeader = document.createElement("h1");
+
+//     const newContent = document.createTextNode("victory");
+
+//     newHeader.appendChild(newContent);
+
+//     const currentHeader = document.getElementById("h1");
+//     document.body.insertBefore(newHeader, currentHeader);
+// }
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.className = "victory";
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Liza is the champion";
+
+
